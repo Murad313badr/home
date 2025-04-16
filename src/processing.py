f3cdc14f17a7cfbc7e4b502043f820795e82f801
Murad Gasanov <ahmadbashirov99@gmail.com>
2025-04-16 21:59:43 +0300

    add test in main

--- src/processing.py
+++ src/processing.py
@@ -18,3 +18,23 @@ def filter_by_state(operations: List[Dict], state: str = "EXECUTED") -> Union[Li
         return "Операции с данным статусом отсутствуют"
     else:
         return list_of_operation
+
+
+def sort_by_date(operations: List[Dict], flow: bool = True) -> Union[List, str]:
+    """Функция сортирует операции по дате"""
+    unknown_date_list: list = [operation for operation in operations if operation.get("date", "UNKNOWN") == "UNKNOWN"]
+    filter_by_wrong_date_list: list = [
+        operation
+        for operation in operations
+        if get_date(operation.get("date", "UNKNOWN")) == "Проверьте правильность ввода!"
+    ]
+    if len(operations) == 0:
+        return "Отсутствуют данные для обработки! Проверьте правильность ввода!"
+    elif len(unknown_date_list) != 0:
+        return "Дата и время одной или нескольких операций не указаны!"
+    elif len(filter_by_wrong_date_list) != 0:
+        return "Дата и время одной или нескольких операций имеют неверный формат!"
+    else:
+        sort_by_date_list = sorted(operations, key=lambda operation: operation["date"], reverse=flow)
+        return sort_by_date_list
+
