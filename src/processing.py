bbde0dd368db9410843d1c4e07233a9be4bb8ec4
Murad Gasanov <ahmadbashirov99@gmail.com>
2025-04-17 13:50:24 +0300

    corrected version

--- src/processing.py
+++ src/processing.py
@@ -13,19 +13,19 @@ operations = [
 
 def filter_by_state(operations: List[Dict], state: str = "EXECUTED") -> Union[List, str]:
     """Функция фильтрует операций в списке по ключу 'state'"""
-    list_of_operation = [operation for operation in operations if operation.get("state", "UNKNOWN") == state]
+    operation_list = [operation for operation in operations if operation.get("state", "UNKNOWN") == state]
     unknown_status_list = [operation for operation in operations if operation.get("state", "UNKNOWN") == "UNKNOWN"]
     if len(operations) == 0:
         return "Отсутствуют данные для обработки! Проверьте правильность ввода!"
     elif len(unknown_status_list) != 0:
         return "Статус одной или нескольких операций не указан!"
-    elif len(list_of_operation) == 0:
+    elif len(operation_list) == 0:
         return "Операции с данным статусом отсутствуют"
     else:
-        return list_of_operation
+        return operation_list
 
 
-def sort_by_date(operations: List[Dict], flow: bool = True) -> Union[List, str]:
+def sort_by_date(operations: List[Dict], ascending: bool = True) -> Union[List, str]:
     """Функция сортирует операции по дате"""
     unknown_date_list: list = [operation for operation in operations if operation.get("date", "UNKNOWN") == "UNKNOWN"]
     filter_by_wrong_date_list: list = [
@@ -40,14 +40,14 @@ def sort_by_date(operations: List[Dict], flow: bool = True) -> Union[List, str]:
     elif len(filter_by_wrong_date_list) != 0:
         return "Дата и время одной или нескольких операций имеют неверный формат!"
     else:
-        sort_by_date_list = sorted(operations, key=lambda operation: operation["date"], reverse=flow)
+        sort_by_date_list = sorted(operations, key=lambda operation: operation["date"], reverse=ascending)
         return sort_by_date_list
 
 
-list_of_operation = filter_by_state(operations, state="EXECUTED")
+operation_list = filter_by_state(operations, state="EXECUTED")
 
-print(list_of_operation)
+print(operation_list)
 
-sort_by_date_list = sort_by_date(operations, flow=True)
+sort_by_date_list = sort_by_date(operations, ascending=True)
 
 print(sort_by_date_list)
